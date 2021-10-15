a8ff38be3d80a3c7ff1e606ff980c516f87033a3
diatlova <dari.diatlova@gmail.com>
2021-10-15 18:33:52 +0300

    added debugged svd version

--- src/hw1/svd/svd.py
+++ src/hw1/svd/svd.py
@@ -1,111 +1,131 @@
 import pandas as pd
 import numpy as np
 import scipy.sparse as sp
-from operator import itemgetter
-
-# from src.hw1.resources import DATA_PATH
-# from src.hw1.mse_logs import LOGGING_PATH
-
+from sklearn.metrics.pairwise import cosine_distances
 import time
 
 
 class SVD:
-
-    def __init__(self, df: pd.DataFrame, hidden_dim: int = 16):
-        # id mapping
-        self.movie_id_mapping = None
-        self.reversed_movie_id_mapping = None
-        self.user_id_mapping = None
-        self.reversed_user_id_mapping = None
-
-        self.unique_user_count = df.user_id.unique().shape[0]
-        self.unique_movie_count = df.movie_id.unique().shape[0]
-
-        self.user_item_matrix = self.get_csr_matrix(df)
-
-        self.item_matrix = np.random.uniform(0, 1, (hidden_dim, self.unique_movie_count))
-        self.user_matrix = np.random.uniform(0, 1, (self.unique_user_count, hidden_dim))
+    def __init__(self, user_item_matrix: sp.csr_matrix, hidden_dim: int = 16):
+        """
+        Classical SVD Algorithm with fit and find top_k similar methods.
+        :param user_item_matrix: sp.csr.csr_matrix of shape n_users X n_movies.
+        :param hidden_dim: int, hidden size of space where user and item embedding will be mapped.
+        """
+        self.user_item_matrix = user_item_matrix
+        self.user_non_zero_idx, self.item_non_zero_idx = self.user_item_matrix.nonzero()
+        self.item_matrix = np.random.uniform(0, 1 / np.sqrt(hidden_dim),
+                                             (hidden_dim, user_item_matrix.toarray().shape[1]))
+        self.user_matrix = np.random.uniform(0, 1 / np.sqrt(hidden_dim),
+                                             (user_item_matrix.toarray().shape[0], hidden_dim))
 
         # overall average rating parameter for regularization
-        self.mu = 0 #df.rating.mean()
-        self.user_bias = np.zeros(self.unique_user_count, dtype=np.float32)
-        self.item_bias = np.zeros(self.unique_movie_count, dtype=np.float32)
+        self.user_bias = np.zeros(user_item_matrix.toarray().shape[0], dtype=np.float32)
+        self.item_bias = np.zeros(user_item_matrix.toarray().shape[1], dtype=np.float32)
 
-    def get_csr_matrix(self, df: pd.DataFrame):
+    def _update_params(self, loss_item: float, user_idx: int, item_idx: int, update_bias: bool,
+                       lr: float, gamma: float, beta: float) -> None:
         """
-        Function takes dataframe with column "user_id" and "movie_id" and created csr matrix filled with ratings.
-        :param df: pd.DataFrame with at least 2 columns to construct csr matrix
+        Function updates user, item vector and user_bias, item_bias vectors.
+        :param loss_item: float – prediction error.
+        :param user_idx: id of user from user/item pair.
+        :param item_idx: if of item from user/item pair.
+        :param update_bias: bool: if true, learn user and bias vector.
+        :param lr: float: learning rate used for updating all 4 vectors.
+        :param gamma: float: hyper parameter used for updating user and item vectors.
+        :param beta: float: hyper parameter used for updating user and item bias vectors.
         :return: None
         """
-        ratings_df = df.sort_values('user_id')
 
-        # mapping for movie ids so then real movie_id == its column index in user_item matrix
-        self.movie_id_mapping = dict(zip(sorted(ratings_df.movie_id.unique()), np.arange(self.unique_movie_count)))
-        self.reversed_movie_id_mapping = dict(
-            zip(np.arange(self.unique_movie_count), sorted(ratings_df.movie_id.unique())))
+        self.user_matrix[user_idx, :] -= lr * (
+                loss_item * self.item_matrix[:, item_idx].T + gamma * self.user_matrix[user_idx, :])
+        self.item_matrix[:, item_idx] -= lr * (
+                loss_item * self.user_matrix[user_idx, :].T + gamma * self.item_matrix[:, item_idx])
+        if update_bias:
+            self.user_bias[user_idx] -= lr * (loss_item + beta * self.user_bias[user_idx])
+            self.item_bias[item_idx] -= lr * (loss_item + beta * self.item_bias[item_idx])
 
-        # mapping for user ids, real user_id == its row index in user_item matrix
-        self.user_id_mapping = dict(zip(sorted(ratings_df.user_id.unique()), np.arange(self.unique_user_count)))
-        self.reversed_user_id_mapping = dict(
-            zip(np.arange(self.unique_user_count), sorted(ratings_df.user_id.unique())))
-        self.user_item_matrix = sp.csr_matrix((ratings_df.rating,
-                                               (itemgetter(*ratings_df.user_id)(self.user_id_mapping),
-                                                itemgetter(*ratings_df.movie_id)(self.movie_id_mapping))),
-                                              shape=(self.unique_user_count, self.unique_movie_count))
+    def _loss(self, prediction: float, score: int, user_bias: float, item_bias: float) -> float:
+        """
+        Function calculates current score error on prediction stage.
+        :param prediction: float: current score computed by user / item matrix multiplication.
+        :param score: int: real score from given csr matrix.
+        :param user_bias: float: user bias value for the given user.
+        :param item_bias: float: item bias value for the given item.
+        :return: float: err
+        """
+        loss_item = prediction + user_bias + item_bias - score
+        return loss_item
 
-        return self.user_item_matrix
+    def mse(self) -> float:
+        """
+        Method returns mse computed on all non_zero values from the target matrix.
+        :return: float: mean squared error.
+        """
+        current_prediction_matrix = np.dot(self.user_matrix, self.item_matrix)
+        return np.mean(np.square(current_prediction_matrix[self.user_non_zero_idx, self.item_non_zero_idx]
+                                 - self.user_item_matrix.toarray()[self.user_non_zero_idx, self.item_non_zero_idx]))
 
-    def loss(self, scores: np.array, predictions: np.array, user_bias: np.array, item_bias: np.array) -> np.ndarray:
-        mse = predictions + user_bias + item_bias + self.mu - scores
-        return mse
+    def fit(self, lr: float = 0.01, gamma: float = 0.001, beta: float = 100., epochs: int = 20,
+            update_bias: bool = True, num_samples_to_update: int = None, logging_path: str = None):
+        """
+        Basic training loop implementation.
+        :param lr: float: learning rate used for updating all 4 vectors.
+        :param gamma: float: hyper parameter used for updating user and item vectors.
+        :param beta: float: hyper parameter used for updating user and item bias vectors.
+        :param epochs: int: total number of epochs to use for updating user/item pairs.
+        :param update_bias: bool: if true, learn user and bias vector.
+        :param num_samples_to_update: int, if not set – equal to all non zero values in the given matrix, so all pairs
+        will bi used for learning during the epoch.
+        :param logging_path: path to the folder to write csv file with mse computed on each epoch.
+        :return: Tuple[np.ndarray, np.ndarray] – user and item matrices. User matrix n_users x hidden_dim, item matrix:
+        hidden_dim x n_items.
+        """
+        if not num_samples_to_update:
+            num_samples_to_update = self.user_non_zero_idx.shape[0]
 
-    def fit(self, eps: float = 5e-1, max_iter: int = 3_000, n_samples_to_optimize: int = 500, lr: float = 1e-3,
-            gamma: float = 0.01, logging_path: str = None):
-        mse = 1
-        current_iter = 0
+        assert num_samples_to_update <= self.user_non_zero_idx.shape[0], \
+            f"Maximum value for num_samples_to_update param  is {self.user_non_zero_idx.shape[0]}."
+
         print('Start fitting the model...')
         start_time = time.time()
         loss_logger = []
+        user_item_matrix = self.user_item_matrix.toarray()
 
-        while current_iter < max_iter and np.mean(np.sqrt(np.square(mse))) > eps:
-            current_iter += 1
-            i = np.random.choice(list(self.user_id_mapping.values()), n_samples_to_optimize)
-            j = np.random.choice(list(self.movie_id_mapping.values()), n_samples_to_optimize)
+        for epoch in range(epochs):
+            # sample n pairs to update on each epoch
+            pairs_idx = np.random.choice(np.arange(self.item_non_zero_idx.shape[0]), num_samples_to_update)
+            for i, idx in enumerate(pairs_idx):
+                user_idx, item_idx = self.user_non_zero_idx[idx], self.item_non_zero_idx[idx]
+                predicted_score = np.dot(self.user_matrix[user_idx, :], self.item_matrix[:, item_idx])
+                target_score = user_item_matrix[user_idx][item_idx]
+                loss_item = self._loss(
+                    predicted_score, target_score, self.user_bias[user_idx], self.item_bias[item_idx])
+                self._update_params(loss_item, user_idx, item_idx, update_bias, lr, gamma, beta)
+            print(f"Wow, {epoch}th is fitted in {int(time.time() - start_time) // 60} minutes!")
+            mse = self.mse()
+            loss_logger.append(mse)
+            print(f"Epoch: {epoch}, mse: {mse}.")
 
-            target_scores = self.user_item_matrix.toarray()[i, j]
-            predicted_scores = np.einsum('ij, ij -> i', self.user_matrix[i, :], self.item_matrix[:, j].T)
-
-            user_vector_regularization = gamma * np.linalg.norm(self.user_matrix[i, :], axis=1)
-            item_vector_regularization = gamma * np.linalg.norm(self.item_matrix[:, j], axis=0)
-            user_bias_regularization = gamma * np.sqrt(np.square(self.user_bias[i]))
-            item_bias_regularization = gamma * np.sqrt(np.square(self.item_bias[j]))
-
-            mse = self.loss(target_scores, predicted_scores, self.user_bias[i], self.item_bias[j])
-            loss_logger.append(np.mean(np.sqrt(np.square(mse))))
-
-            # update weights
-            self.user_bias[i] -= np.clip(lr * (mse + (gamma * self.user_bias[i])), a_min=-4, a_max=4) + user_bias_regularization
-            self.item_bias[j] -= np.clip(lr * (mse + (gamma * self.item_bias[j])), a_min=-4, a_max=4) + item_bias_regularization
-
-            self.user_matrix[i, :] -= lr * (np.einsum(
-                'i, ij -> ij', mse + user_vector_regularization, self.item_matrix[:, j].T) + (gamma * self.user_matrix[i, :]))
-            self.item_matrix[:, j] -= lr * (np.einsum(
-                'i, ij -> ij', mse + item_vector_regularization, self.user_matrix[i, :]).T + (gamma * self.item_matrix[:, j]))
-
-        # mse_logs loss value
         if logging_path:
-            loss_logger_df = pd.DataFrame({'iter': range(len(loss_logger)), 'mse': loss_logger})
-            loss_logger_df.to_csv(f"{logging_path}/lr{lr}_max_iter{max_iter}_hidden_dim{self.user_matrix.shape[1]}"
-                                  f"_samples{n_samples_to_optimize}",
-                                  index=False)
-        print(f'Model fitted in {int(time.time() - start_time)} seconds.')
-        print(f'MSE on iteration {current_iter}: {np.round(loss_logger[-1], 2)}.')
+            loss_logger_df = pd.DataFrame({'epoch': range(len(loss_logger)), 'mse': loss_logger})
+            loss_logger_df.to_csv(f"{logging_path}/lr{lr}_epoch{epochs}_hidden_dim{self.user_matrix.shape[1]}"
+                                  f"_samples{num_samples_to_update}", index=False)
+
+        print(f'Model fitted in {int(time.time() - start_time) / 60} minutes.')
         return self.user_matrix, self.item_matrix
 
-
-# ratings = pd.read_csv(f'{DATA_PATH.parent}/ratings.dat', delimiter='::', header=None,
-#                       names=['user_id', 'movie_id', 'rating', 'timestamp'],
-#                       usecols=['user_id', 'movie_id', 'rating'], engine='python')
-#
-# svd_model = SVD(ratings)
-# user_matrix, item_matrix = svd_model.fit()
+    def get_k_similar_movies(self, item_ids: np.array, n: int = 5, metric: str='cosine'):
+        """
+        Function take an 1d array of item_ids and return 2d with the list of the most similar movies.
+        :param item_ids: np.ndarray with movie indices
+        :param n: int: number of items to search among similar.
+        :param metric: str: dot, otherwise cosine distance.
+        :return: 2d np.ndarray of shape n_indices x n
+        """
+        if metric == "dot":
+            predicted_scores = np.dot(self.item_matrix[:, item_ids].T, self.item_matrix)
+        else:
+            predicted_scores = 1 - cosine_distances(self.item_matrix[:, item_ids].T, self.item_matrix.T)
+        predictions = np.argsort(-predicted_scores)[:, :n + 1]
+        return predictions[:, 1:]
