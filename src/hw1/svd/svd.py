074a002edcef8ee9061dea93908207f947b81b61
diatlova <dari.diatlova@gmail.com>
2021-10-17 23:51:08 +0300

    updated svd via super class

--- src/hw1/svd/svd.py
+++ src/hw1/svd/svd.py
@@ -1,28 +1,10 @@
 import pandas as pd
 import numpy as np
-import scipy.sparse as sp
-from sklearn.metrics.pairwise import cosine_distances
 import time
+from src.hw1.util import MatrixFactorization
 
 
-class SVD:
-    def __init__(self, user_item_matrix: sp.csr_matrix, hidden_dim: int = 16):
-        """
-        Classical SVD Algorithm with fit and find top_k similar methods.
-        :param user_item_matrix: sp.csr.csr_matrix of shape n_users X n_movies.
-        :param hidden_dim: int, hidden size of space where user and item embedding will be mapped.
-        """
-        self.user_item_matrix = user_item_matrix
-        self.user_non_zero_idx, self.item_non_zero_idx = self.user_item_matrix.nonzero()
-        self.item_matrix = np.random.uniform(0, 1 / np.sqrt(hidden_dim),
-                                             (hidden_dim, user_item_matrix.toarray().shape[1]))
-        self.user_matrix = np.random.uniform(0, 1 / np.sqrt(hidden_dim),
-                                             (user_item_matrix.toarray().shape[0], hidden_dim))
-
-        # overall average rating parameter for regularization
-        self.user_bias = np.zeros(user_item_matrix.toarray().shape[0], dtype=np.float32)
-        self.item_bias = np.zeros(user_item_matrix.toarray().shape[1], dtype=np.float32)
-
+class SVD(MatrixFactorization):
     def _update_params(self, loss_item: float, user_idx: int, item_idx: int, update_bias: bool,
                        lr: float, gamma: float, beta: float) -> None:
         """
@@ -56,15 +38,6 @@ class SVD:
         """
         loss_item = prediction + user_bias + item_bias - score
         return loss_item
-
-    def mse(self) -> float:
-        """
-        Method returns mse computed on all non_zero values from the target matrix.
-        :return: float: mean squared error.
-        """
-        current_prediction_matrix = np.dot(self.user_matrix, self.item_matrix)
-        return np.mean(np.square(current_prediction_matrix[self.user_non_zero_idx, self.item_non_zero_idx]
-                                 - self.user_item_matrix.toarray()[self.user_non_zero_idx, self.item_non_zero_idx]))
 
     def fit(self, lr: float = 0.01, gamma: float = 0.001, beta: float = 100., epochs: int = 20,
             update_bias: bool = True, num_samples_to_update: int = None, logging_path: str = None):
@@ -114,18 +87,3 @@ class SVD:
 
         print(f'Model fitted in {int(time.time() - start_time) / 60} minutes.')
         return self.user_matrix, self.item_matrix
-
-    def get_k_similar_movies(self, item_ids: np.array, n: int = 5, metric: str='cosine'):
-        """
-        Function take an 1d array of item_ids and return 2d with the list of the most similar movies.
-        :param item_ids: np.ndarray with movie indices
-        :param n: int: number of items to search among similar.
-        :param metric: str: dot, otherwise cosine distance.
-        :return: 2d np.ndarray of shape n_indices x n
-        """
-        if metric == "dot":
-            predicted_scores = np.dot(self.item_matrix[:, item_ids].T, self.item_matrix)
-        else:
-            predicted_scores = 1 - cosine_distances(self.item_matrix[:, item_ids].T, self.item_matrix.T)
-        predictions = np.argsort(-predicted_scores)[:, :n + 1]
-        return predictions[:, 1:]
